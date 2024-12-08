3af27a355e6acd9b14688601bc7ab1c5eff4e797
陈兴 <2938244975@qq.com>
2024-12-08 23:03:23 +0800

    主页视频背景

---
+++ pages/index/index.vue
@@ -0,0 +1,53 @@
+<template>
+	<view class="login-box">
+		<view class="video-background">
+			<video src="https://mp-56351f42-ba58-4baf-bbc7-144b70e6e6eb.cdn.bspapp.com/video/00068.mp4"
+				object-fit="cover"
+				:autoplay="true" 
+				:controls="false" 
+				:muted="true" 
+				:loop="true" 
+				id="videoRef"
+				class="video">
+			</video>
+		</view>
+	</view>
+	
+</template>
+ 
+<script>
+	export default {
+		data(){
+			return {
+				VideoContext: {}
+			}
+		},
+		onShow() {
+            // 当app切到后台再切回前台的时候会触发onShow这个时候视频应该继续播放，不这样做视频会暂停的
+			this.$nextTick(() => {
+				this.VideoContext = uni.createVideoContext('videoRef');
+				this.VideoContext.play()
+			})
+		},
+	}
+</script>
+ 
+<style lang="scss">
+	.login-box{
+		position: relative;
+		height: 100vh;
+		overflow: hidden;
+		.video-background{
+			position: absolute;
+			top: 0;
+			right: 0;
+			left: 0;
+			bottom: 0;
+			z-index: -1;
+			.video{
+				width: 100%;
+				height: 100%;
+			}
+		}
+	}
+</style>
